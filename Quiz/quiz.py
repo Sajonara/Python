658562a462073c2ef5c60ee87177d7d2a56c53c4
atrust@sajonara.de <atrust@sajonara.de>
2022-08-07 15:58:23 +0200

    Added play method to Quiz class, which prints out question and 4 possible answers.

--- Quiz/quiz.py
+++ Quiz/quiz.py
@@ -2,6 +2,18 @@ class Quiz:
     questions = {}
     answers = {}
 
+    def play_quiz(self):
+        for question in self.questions:
+            frage = """
+            Die Frage lautet: {frage}
+            """.format(frage = self.questions[question])
+            print(frage)
+            antwort = "Die Antwortmöglichkeiten lauten:\n"
+            for answer in self.answers:
+                if self.answers[answer][0] == question:
+                    antwort += self.answers[answer][1]+"\n"
+            print(antwort)
+
 class Question:
     question_id = 0
 
@@ -27,13 +39,28 @@ class Answer:
         Answer.answer_id += 1
         self.id = Answer.answer_id
         Quiz.answers[self.id] = [question_id, self.answer]
+    
+    def __repr__(self):
+        description = """
+        This is answer number {id}. It belongs to question number {question_id}.
+        It reads: "{answer}"
+        """.format(id = self.id, question_id = self.question_id, answer = self.answer)
+        return description
 
 
 question_1 = Question("Wo steht der \"schiefe Turm\"?")
-print(question_1)
+#print(question_1)
+
 
 answer_1 = Answer(1, "Pisa")
-print(answer_1)
+answer_2 = Answer(1, "Mailand")
+answer_3 = Answer(1, "Rom")
+answer_4 = Answer(1, "Verona")
+#print(answer_1)
 
 print(Quiz.questions)
 print(Quiz.answers)
+
+my_quiz = Quiz()
+
+my_quiz.play_quiz()
